0433b6231b5be91c0eef107fc3bcae28e83759fa
gitalpine <ostmail@163.com>
2016-05-10 17:44:58 +0800

    Initial Commit

---
+++ AutoTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoTest
+//
+//  Created by zhangyun on 16/5/10.
+//  Copyright © 2016年 USC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
